dee413e32c5dd96866215a4965b4c576fd789cc3
sebastientromp <sebastien.tromp@gmail.com>
2021-04-29 11:02:30 +0200

    remove error-eating try/catch

--- src/retrieve-duels-single-run.ts
+++ src/retrieve-duels-single-run.ts
@@ -14,91 +14,79 @@ export default async (event): Promise<any> => {
 		'Access-Control-Allow-Methods': 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT',
 		'Access-Control-Allow-Origin': event.headers?.Origin || event.headers?.origin || '*',
 	};
-	try {
-		console.log('processing event', event);
-		const escape = SqlString.escape;
-		const runId = event.pathParameters?.proxy;
+	console.log('processing event', event);
+	const escape = SqlString.escape;
+	const runId = event.pathParameters?.proxy;
 
-		const mysql = await getConnection();
+	const mysql = await getConnection();
 
-		const lootQuery = `
+	const lootQuery = `
 			SELECT * FROM dungeon_run_loot_info
 			WHERE runId = ${escape(runId)}
 		`;
-		console.log('running query', lootQuery);
-		const lootDbResults: readonly InternalLootInfo[] = await mysql.query(lootQuery);
-		console.log(
-			'executed query',
-			lootDbResults && lootDbResults.length,
-			lootDbResults && lootDbResults.length > 0 && lootDbResults[0],
-		);
+	console.log('running query', lootQuery);
+	const lootDbResults: readonly InternalLootInfo[] = await mysql.query(lootQuery);
+	console.log(
+		'executed query',
+		lootDbResults && lootDbResults.length,
+		lootDbResults && lootDbResults.length > 0 && lootDbResults[0],
+	);
 
-		const gameStatsQuery = `
+	const gameStatsQuery = `
 			SELECT t1.*, t2.duelsRunId as currentDuelsRunId FROM replay_summary t1
 			INNER JOIN replay_summary_secondary_data t2 on t1.reviewId = t2.reviewId
 			WHERE duelsRunId = ${escape(runId)}
 		`;
-		console.log('running query', gameStatsQuery);
-		const gameStatDbResults: readonly GameStatQueryResult[] = await mysql.query(gameStatsQuery);
-		console.log(
-			'executed query',
-			gameStatDbResults && gameStatDbResults.length,
-			gameStatDbResults && gameStatDbResults.length > 0 && gameStatDbResults[0],
-		);
-		await mysql.end();
+	console.log('running query', gameStatsQuery);
+	const gameStatDbResults: readonly GameStatQueryResult[] = await mysql.query(gameStatsQuery);
+	console.log(
+		'executed query',
+		gameStatDbResults && gameStatDbResults.length,
+		gameStatDbResults && gameStatDbResults.length > 0 && gameStatDbResults[0],
+	);
+	await mysql.end();
 
-		const lootResults =
-			!lootDbResults || lootDbResults.length === 0
-				? []
-				: lootDbResults.map(
-						result =>
-							({
-								...result,
-								creationTimestamp: Date.parse(result.creationDate),
-								option1Contents: result.option1Contents ? result.option1Contents.split(',') : [],
-								option2Contents: result.option2Contents ? result.option2Contents.split(',') : [],
-								option3Contents: result.option3Contents ? result.option3Contents.split(',') : [],
-							} as DuelsRunInfo),
-				  );
-		console.log('results', lootResults);
+	const lootResults =
+		!lootDbResults || lootDbResults.length === 0
+			? []
+			: lootDbResults.map(
+					result =>
+						({
+							...result,
+							creationTimestamp: Date.parse(result.creationDate),
+							option1Contents: result.option1Contents ? result.option1Contents.split(',') : [],
+							option2Contents: result.option2Contents ? result.option2Contents.split(',') : [],
+							option3Contents: result.option3Contents ? result.option3Contents.split(',') : [],
+						} as DuelsRunInfo),
+			  );
+	console.log('results', lootResults);
 
-		const gameStatResults =
-			!gameStatDbResults || gameStatDbResults.length === 0
-				? []
-				: gameStatDbResults.map(result => ({
-						...result,
-						creationTimestamp: Date.parse(result.creationDate),
-				  }));
+	const gameStatResults =
+		!gameStatDbResults || gameStatDbResults.length === 0
+			? []
+			: gameStatDbResults.map(result => ({
+					...result,
+					creationTimestamp: Date.parse(result.creationDate),
+			  }));
 
-		const results: (GameStatQueryResult | DuelsRunInfo)[] = [...lootResults, ...gameStatResults].sort(
-			(a, b) => a.creationTimestamp - b.creationTimestamp,
-		);
+	const results: (GameStatQueryResult | DuelsRunInfo)[] = [...lootResults, ...gameStatResults].sort(
+		(a, b) => a.creationTimestamp - b.creationTimestamp,
+	);
 
-		const stringResults = JSON.stringify({ results: results });
-		const gzippedResults = gzipSync(stringResults).toString('base64');
-		console.log('compressed', stringResults.length, gzippedResults.length);
-		const response = {
-			statusCode: 200,
-			isBase64Encoded: true,
-			body: gzippedResults,
-			headers: {
-				'Content-Type': 'text/html',
-				'Content-Encoding': 'gzip',
-			},
-		};
-		console.log('sending back success reponse');
-		return response;
-	} catch (e) {
-		console.error('issue getting runs info', e);
-		const response = {
-			statusCode: 500,
-			isBase64Encoded: false,
-			body: null,
-			headers: headers,
-		};
-		console.log('sending back error reponse', response);
-		return response;
-	}
+	const stringResults = JSON.stringify({ results: results });
+	const gzippedResults = gzipSync(stringResults).toString('base64');
+	console.log('compressed', stringResults.length, gzippedResults.length);
+	const response = {
+		statusCode: 200,
+		isBase64Encoded: true,
+		body: gzippedResults,
+		headers: {
+			'Content-Type': 'text/html',
+			'Content-Encoding': 'gzip',
+		},
+	};
+	console.log('sending back success reponse');
+	return response;
 };
 
 interface InternalLootInfo {
